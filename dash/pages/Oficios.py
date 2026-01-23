e89289be7ece5f49ac5fcada172a0f2e5586b1ab
cmdi4s <devvdias@gmail.com>
2026-01-23 10:02:35 -0300

    update connection to db

--- dash/pages/Oficios.py
+++ dash/pages/Oficios.py
@@ -1,34 +1,17 @@
-from sqlalchemy import create_engine
 import streamlit as st
 import pandas as pd
-from dotenv import load_dotenv
+from sqlalchemy import create_engine
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
-USER = os.getenv("DB_USER")
-PASSWORD = os.getenv("DB_PASSWORD")
-HOST = os.getenv("DB_HOST")
-PORT = os.getenv("DB_PORT")
-DBNAME = os.getenv("DB_NAME")
-
-engine = create_engine(
-    f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}",
-    connect_args={
-        "sslmode": "require"
-    },
-    pool_size=5,
-    max_overflow=2,
-    pool_timeout=30,
-    pool_recycle=1800
-)
-
-
 @st.cache_data(ttl=300)
-def load_data(query):
-    return pd.read_sql(query, engine)
+def load_data():
+    return pd.read_sql("SELECT * FROM test_table", engine)
 
-query = "SELECT * FROM document_control"
-df = load_data(query)
+engine = create_engine(os.getenv("SUPABASE_DB_URL"))
 
+
+df = load_data()
 st.dataframe(df)
