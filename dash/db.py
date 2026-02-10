b1d8d9fc06ad873343dbb7627a541b3fad9e8d62
cmdi4s <devvdias@gmail.com>
2026-02-10 15:36:25 -0300

    change db

--- dash/db.py
+++ dash/db.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_engine():
-    db_url = os.getenv("SUPABASE_DB_URL")
+    db_url = os.getenv("DB_URL")
     return create_engine(db_url, pool_pre_ping=True)
