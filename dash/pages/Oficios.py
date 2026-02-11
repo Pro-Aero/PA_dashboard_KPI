bccbfcd1e19e3df087e415f05b728785741c4428
cmdi4s <devvdias@gmail.com>
2026-02-11 10:45:11 -0300

    metrics add

--- dash/pages/Oficios.py
+++ dash/pages/Oficios.py
@@ -13,4 +13,24 @@ def load_data():
 engine = create_engine(st.secrets["DB_URL"])
 
 df = load_data()
+
+st.subheader("Resumo de Documentos")
+
+total = len(df)
+st.metric("Total", total)
+
+st.divider();
+
+
+status_count = df["Situação"].value_counts()
+
+cols = st.columns(len(status_count))
+
+for col, (status, quantidade) in zip (cols, status_count.items()):
+    with col:
+        st.metric(
+            label=status,
+            value=quantidade,
+        )
+
 st.dataframe(df)
