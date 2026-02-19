e4f1b8bf0d72b4d9cf601900f155d5b34e2ead94
cmdi4s <devvdias@gmail.com>
2026-02-19 14:18:08 -0300

    change ETL workflow

--- etl/main.py
+++ etl/main.py
@@ -1,16 +1,51 @@
-from extract import extract_excel
-from transform import transform_document_control
-from load import load_dataframe
+import pandas as pd
+import requests
+import os
+from dotenv import load_dotenv
+from supabase import create_client
 
-PATH = "document_control.xlsx"
-TABLE = "document_control"
-UNIQUE_KEY = "id"
 
-def main():
-    df = extract_excel(PATH)
-    df = transform_document_control(df)
-    load_dataframe(df, TABLE, UNIQUE_KEY)
-    print("ETL finalizado com sucesso")
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+TABLE = os.getenv("SUPABASE_TABLE")
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+TENANT_ID = os.getenv("TENANT_ID")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+FILE_PATH = os.getenv("ONEDRIVE_FILE_PATH")
+
+
+def get_acess_token():
+
+    url = f"https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token"
+
+    payload = {
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "scope": "https://graph.microsoft.com/.default",
+        "grant_type": "client_credentials",
+    }
+
+    response = requests.post(url, data=payload)
+    response.raise_for_status()
+
+    return response.json()["access_token"]
+
+def load_dataframe_to_supabase_upsert(df):
+    data = df.to_dict("records")
+    response = supabase.table(TABLE).upsert(data).execute()
+    return response
+
+def run_etl():
+    # EXTRACT
+
+    # TRANSFORM
+
+    # LOAD
+    load_dataframe_to_supabase_upsert(df)
+
+    return df
